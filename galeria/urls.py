28d55719e99a55770c9ca188626f3b973693b3b0
Everyzin <matheus.siqueira@a.fecaf.com.br>
2024-04-10 11:53:49 -0300

    atualização de projeto

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from galeria.views import buscar, index, imagem
+from galeria.views import buscar, index, imagem, filtrar
 
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
-    path('buscar', buscar, name='buscar'),
+    path('buscar/', buscar, name='buscar'),
+    path('filtrar/<filtro>', filtrar, name='filtrar')
 ]
